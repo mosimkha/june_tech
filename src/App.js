74a6a76e6a502c81c553778a3b9e6e677ef76671
mosimkha <pathan.mohsin73@gmail.com>
2022-06-13 11:08:28 +0530

    techAvidus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Delete from "./component/Delete";
+import { Footer } from "./component/Footer";
+import NavBar from "./component/NavBar";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Crud from "./component/Crud";
+import Edit from "./component/Edit";
+import Add from "./component/Add";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Crud />
+      <br />
+      <br />
+      <Edit />
+      <br />
+      <Delete />
+      <br />
+
+      <Add />
+    </>
   );
 }
 
---
+++ src/component/Add.js
@@ -0,0 +1,137 @@
+import React from "react";
+
+function Add() {
+  return (
+    <>
+      {/* s */}
+
+      {/* <!-- Button trigger modal --> */}
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Add button
+      </button>
+
+      {/* <!-- Modal --> */}
+
+      <div
+        className="modal fade  modal-xl"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-xl modal-dialog-scrollable ">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add Product
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+
+            <div className="modal-body">
+              <form>
+                Picture
+                <div className="input-group mb-3">
+                  {/* <label className="input-group-text" for="inputGroupFile01">Upload</label> */}
+                  <input
+                    type="file"
+                    className="form-control"
+                    id="inputGroupFile01"
+                  />
+                </div>
+                {/* Product name */}
+                <div className="mb-3">
+                  <label for="recipient-name" className="col-form-label">
+                    Product name:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="message-text" className="col-form-label">
+                    Detail:
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message-text"
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <label for="recipient-name" className="col-form-label">
+                    Price:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-name"
+                  />
+                </div>
+                <label>Quantity</label>
+                <div class="input-group mb-2">
+                  <button type="button" class="input-group-text">
+                    -
+                  </button>
+                  <input
+                    type="text"
+                    class="form-control"
+                    aria-label="Amount (to the nearest dollar)"
+                  />
+                  <button type="button" class="input-group-text">
+                    +
+                  </button>
+                </div>
+                <label for="recipient-name" className="col-form-label">
+                  Total Price:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="recipient-name"
+                />
+                <label for="recipient-name" className="col-form-label">
+                  Date:
+                </label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="recipient-name"
+                />
+              </form>
+            </div>
+
+            <div className="modal-footer">
+              <button type="button" className="btn btn-success">
+                Add
+              </button>
+
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* e */}
+    </>
+  );
+}
+
+export default Add;
