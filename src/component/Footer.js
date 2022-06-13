1c8bcf359b733daa88597aaf0ad9331c544db31f
Mohsinkhan <57690391+mosimkha@users.noreply.github.com>
2022-06-13 11:18:18 +0530

    Delete Footer.js

--- src/component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <footer className="bg-primary text-light py-1">
-      <p className="text-center">Copyright &copy; Album.com</p>
-    </footer>
-  );
-};
